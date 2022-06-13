ffecc9eda278547cce48abb97cf8ca9459e1006d
okubon <oliver.kubon@smail.th-koeln.de>
2022-06-13 14:11:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 from snorkel.labeling import labeling_function
 import pandas as pd
 import re
+from collections import Counter
 
 
 def load_data(path):
@@ -9,10 +10,26 @@ def load_data(path):
     root = tree.getroot()
     return root
 
+
+def clean_list(input):
+    output = []
+    for item in input:
+        l = len(item)
+        new_item = item[:l-1]
+        output.append(new_item)
+    return output
+
+
+@labeling_function()
+def beitrag(txt):
+    # find all: "name (ort) (partei):" or "name (partei):" or "name (partei/partei):"
+    list = re.findall("([a-zA-ZÄäÜüÖö]+\s\([^)]*\)\s\([^)]*\):|[a-zA-ZÄäÜüÖö]+\s\([^)]*\):)", txt)
+    list = clean_list(list)
+    return list
+
 def main():
     path = './testdaten/10001.xml'
     root = load_data(path)
-
 
     # get content from xml file
     wahlperiode = root[0].text
@@ -22,21 +39,12 @@ def main():
     titel = root[4].text
     text = root[5].text
 
+    # get count of beiträge
+    results = beitrag(text)
+    counts = Counter(results)
 
-    # crate label functions
-    @labeling_function()
-    def beitrag(txt):
-        # find all: "name (ort) (partei):" or "name (partei):" or "name (partei/partei):"
-        list = re.findall("([a-zA-ZÄäÜüÖö]+\s\([^)]*\)\s\([^)]*\):|[a-zA-ZÄäÜüÖö]+\s\([^)]*\):)", txt)
-        return list
-
-    # empty df to store results from label function
-    df = pd.DataFrame()
-
-    list = beitrag(text)
-    count = list.count(value)
-
-    print(count)
+    df = pd.DataFrame.from_dict(counts, orient = 'index').reset_index()
+    print(df)
 
 
 main()

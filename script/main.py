2bac8e3d2308f8e1812380221e363894dac1e8f2
xxjuly <janina.blum@smail.th-koeln.de>
2022-08-25 12:40:06 +0200

    fighting logic errors with all my might, spoiler: i'm winning

--- script/main.py
+++ script/main.py
@@ -9,42 +9,30 @@ import logging
 # alternative to spamming your terminal, used to check on performance and errors
 logging.basicConfig(
     format='%(asctime)s: %(message)s', 
-    datefmt='%I:%M:%S', 
-    filename='filetime.log', 
+    datefmt='%d/%m/%Y %I:%M:%S', 
+    filename='script/filetime.log', 
     filemode="w", 
     encoding='utf-8', 
     level=logging.INFO
 )
 
-def type_of_speech(str):
+def merge_all_csv():
     '''
-    Checks if the part contains a RB or KO.
+    For performance reasons every period has its seperate csv. 
+    This function merges all of them. Note: the output csv might be
+    to big for some pcs or tools to handle.
     '''
-    # RegEx for the different formats of RB
-    rb_regex = {
-        'base': "([a-zA-ZÄäÜüÖöß]+\s\([^)]*\)\s\([^)]*\):|[a-zA-ZÄäÜüÖöß]+\s\([^)]*\).*?:)",
-        'title': "Dr\..*[a-zA-ZÄäÜüÖöß]+.*:"
-    }
+    rb_path = "output/rb/"
+    rb_all_files = glob.glob(os.path.join(rb_path, "*.csv"))
+    rb_df_from_each_file = (pd.read_csv(f, sep=',') for f in rb_all_files)
+    rb_df_merged = pd.concat(rb_df_from_each_file, ignore_index=True)
+    rb_df_merged.to_csv("output/rb_merged.csv")
 
-    # RegEx for the different formats of KO
-    ko_regex = {
-        "base": "[a-zA-Z]+.*\[.*\]:"
-    }
-    
-    if re.match(rb_regex["base"], str):
-        match = re.match(rb_regex["base"], str)
-        type = 'rb'    
-    elif re.match(rb_regex["title"], str):
-        match = re.match(rb_regex["title"], str)
-        type = 'rb'    
-    elif re.match(ko_regex["base"], str):
-        match = re.match(ko_regex["base"], str)
-        type = 'ko'
-    else:
-        match = ["Null"]
-        type = "Null"
-
-    return type, match[0]
+    ko_path = "output/ko/"
+    ko_all_files = glob.glob(os.path.join(ko_path, "*.csv"))
+    ko_df_from_each_file = (pd.read_csv(f, sep=',') for f in ko_all_files)
+    ko_df_merged = pd.concat(ko_df_from_each_file, ignore_index=True)
+    ko_df_merged.to_csv("output/ko_merged.csv")
 
 def clean_rb_df(df):
     '''
@@ -106,6 +94,36 @@ def clean_ko_item(input):
     
     return input
 
+def type_of_speech(sentence):
+    '''
+    Checks if the part contains a RB or KO.
+    '''
+    # RegEx for the different formats of RB
+    rb_regex = {
+        'base': "([a-zA-ZÄäÜüÖöß]+\s\([^)]*\)\s\([^)]*\):|[a-zA-ZÄäÜüÖöß]+\s\([^)]*\).*?:)",
+        'title': "Dr\..*[a-zA-ZÄäÜüÖöß]+.*:"
+    }
+
+    # RegEx for the different formats of KO
+    ko_regex = {
+        "base": "[a-zA-Z]+.*\[.*\]:"
+    }
+    
+    if re.match(rb_regex["base"], sentence):
+        match = re.match(rb_regex["base"], sentence)
+        type = 'rb'    
+    elif re.match(rb_regex["title"], sentence):
+        match = re.match(rb_regex["title"], sentence)
+        type = 'rb'    
+    elif re.match(ko_regex["base"], sentence):
+        match = re.match(ko_regex["base"], sentence)
+        type = 'ko'
+    else:
+        match = ["Null"]
+        type = "Null"
+
+    return type, match[0]
+
 def extract_from_protocol(file, rb_ID, rb_dict, ko_dict):
     '''
     Cuts a singular given protocol into parts and extracts the RB and KO.
@@ -123,7 +141,6 @@ def extract_from_protocol(file, rb_ID, rb_dict, ko_dict):
     # iteration through sentences
     for sentence in text:
         type, match = type_of_speech(sentence)
-        # match = clean_rb_item(check[1])
 
         if type == 'rb':
             rb_ID += 1
@@ -139,7 +156,6 @@ def extract_from_protocol(file, rb_ID, rb_dict, ko_dict):
     rb_df['WAHLPERIODE'] = wahlperiode
     rb_df['NR'] = nr
     rb_df['DATUM'] = datum
-    #rb_df = clean_rb_df(rb_df) #TODO 
 
     # create ko df
     ko_df = pd.DataFrame.from_dict(ko_dict, orient = 'index').reset_index()
@@ -152,25 +168,35 @@ def main(dir, rb_ID):
     Walks through the entire process of extracting, assigning, 
     cleaning and saving the data.
     '''
-    # empty dfs to store results for RB and KO
-    RB_df = pd.DataFrame()
-    KO_df = pd.DataFrame()
-
-    # empty dicts to store the results
-    rb_dict = {}
-    ko_dict = {}
-
     dir = 'protokolle_wp_1-12/'
 
     filenumber, total_time, wp = 0, 0, 0
 
+    # empty dfs to store results for RB and KO
+    RB_df = pd.DataFrame(columns=["RB_ID","RB_Name","WAHLPERIODE","NR","DATUM"])
+    KO_df = pd.DataFrame(columns=["RB_ID","KO_Name"])
+
     # iterate through folders and files
     for root, dirs, files in os.walk(dir):  
 
-        logging.info(f' WP{wp} - {filenumber} files - {round(total_time,2)} sec.')
-        filenumber, total_time, wp = 0, 0, 0
+        if (RB_df.shape[0] != 0) and (KO_df.shape[0] != 0):
+
+            # export to csv
+            RB_df.to_csv(rf'output/rb/rb{wp}.csv',encoding='utf-8-sig', index = False)
+            KO_df.to_csv(rf'output/ko/ko{wp}.csv',encoding='utf-8-sig', index = False) 
+                        
+            logging.info(f' WP{wp} - {filenumber} files - {RB_df.shape[0]} RB - {KO_df.shape[0]} KO - {round(total_time,2)} sec.')
+            filenumber, total_time = 0, 0    
+            
+            # empty out dfs for next loop
+            RB_df = RB_df.iloc[0:0]
+            KO_df = KO_df.iloc[0:0]
 
         for name in files:
+
+            # empty dicts to store the results
+            rb_dict = {}
+            ko_dict = {}
 
             filenumber += 1
             t0 = time.time()
@@ -180,43 +206,12 @@ def main(dir, rb_ID):
 
             part_rb_df, part_ko_df, rb_ID, wp = extract_from_protocol(file, rb_ID, rb_dict, ko_dict)
             
-            #Clean_rb_df = clean_rb_df() #TODO 
-
             RB_df = pd.concat([RB_df,part_rb_df])
             KO_df = pd.concat([KO_df,part_ko_df])
-
-            # adjust RB_id per period
-            RB_df['RB_ID'] = "WP" + str(wp) + "_RB" + RB_df['RB_ID'].astype(str)
-            KO_df['RB_ID'] = "WP" + str(wp) + "_RB" + KO_df['RB_ID'].astype(str)
-
-            # export to csv
-            RB_df.to_csv(rf'output/rb/{wp}.csv',encoding='utf-8-sig', index = False)
-            KO_df.to_csv(rf'output/ko/{wp}.csv',encoding='utf-8-sig', index = False)
-            
-            # empty out dfs for next loop
-            RB_df = RB_df.iloc[0:0]
-            KO_df = KO_df.iloc[0:0]
 
             t1 = time.time()
             total_time += t1 - t0 
 
-def merge_all_csv():
-    '''
-    For performance reasons every period has its seperate csv. 
-    This function merges all of them. Note: the output csv might be
-    to big for some pcs or tools to handle.
-    '''
-    rb_path = "output/rb/"
-    rb_all_files = glob.glob(os.path.join(rb_path, "*.csv"))
-    rb_df_from_each_file = (pd.read_csv(f, sep=',') for f in rb_all_files)
-    rb_df_merged = pd.concat(rb_df_from_each_file, ignore_index=True)
-    rb_df_merged.to_csv("output/rb_merged.csv")
-
-    ko_path = "output/ko/"
-    ko_all_files = glob.glob(os.path.join(ko_path, "*.csv"))
-    ko_df_from_each_file = (pd.read_csv(f, sep=',') for f in ko_all_files)
-    ko_df_merged = pd.concat(ko_df_from_each_file, ignore_index=True)
-    ko_df_merged.to_csv("output/ko_merged.csv")
 
 main(dir, 0)
 # commented out because the merged file tends to kill (me and) the data 
